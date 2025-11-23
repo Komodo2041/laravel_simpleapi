3ff154d4f8912f569fbe134195ab09040ad1fa5d
Łukasz Jaroszuk <krortop@gmail.com>
2025-11-23 13:12:12 +0100

    add delete company method

--- front/src/components/CompanyApp.vue
+++ front/src/components/CompanyApp.vue
@@ -8,13 +8,19 @@
                <td>{{c.adress}}</td>
                <td>{{c.city}}</td>
                <td>{{c.nip}}</td>
+              <td>
+                  <a href="#" v-on:click="deleteCompany(c.id)" > x </a>
+              </td>
             </tr>
          </table>
           
         <div class="row">  
           <div class="alert alert-warning col-12" role="alert" v-show="showerror">
              {{error}}
-          </div>  
+          </div>
+          <div class="alert alert-warning col-12" role="alert" v-show="showsuccess">
+             {{success}}
+          </div>            
           <div class="col-12">      
             <form action=""  method="POST" v-on:submit.prevent="addCompany"  >  
               <div class="form-group">
@@ -48,7 +54,7 @@
     }
   
     let company = await loadFirms();
-    console.log(company);
+   
 
 
 export default {
@@ -56,13 +62,15 @@ export default {
   data() {
     return {
       company: company,
-      name: "xxx",
+      name: "",
       city: "",
       postcode: "",
       nip: "",
       adress: "",
       showerror: false,
-      error: ""
+      error: "",
+      success: "",
+      showsuccess: false
     }
   }, methods: {
       addFirm : async function(data) {
@@ -75,6 +83,15 @@ export default {
           }
  
      },
+     deleteFirm : async function(id) {
+        let link = "http://127.0.0.1:8000/api/company/delete/" + id;
+        try {
+            const response = await axios.delete(link);
+            return response.data;
+          } catch (error) {  
+            return error.response.data;  
+          }
+     },     
      addCompany: function() {
  
       let datac = {
@@ -84,17 +101,23 @@ export default {
         nip: this.nip,
         adress: this.adress
       };
-      console.log(datac);
-      let res =  this.addFirm(datac).then(response => {
+   
+      this.addFirm(datac).then(response => {
             const res = response;
  
             if (res.errors && res.errors.length > 0) {
               this.showerror = true;
               this.error =  res.errors.join(", ");
+              this.success = false;
             }  else {
               this.showerror = false;
-               this.error = "";
+              this.error = "";
               this.clearForm(); 
+              this.showsuccess = true;
+              this.success = res.message;
+              loadFirms().then(response => {
+                 this.company = response;
+              })
             }
  
         }).catch(error => {
@@ -110,7 +133,31 @@ export default {
       this.postcode = "";
       this.nip = "";
       this.adress = "";
-    }    
+    },
+    deleteCompany : function(id) {
+          let ok = confirm("Are you sure?");
+          if (ok) {
+            this.deleteFirm(id).then(response => {
+              const res = response;
+              if (res.errors && res.errors.length > 0) {
+                this.showerror = true;
+                this.error =  res.errors.join(", ");
+                this.success = false;
+              }  else {
+                this.showerror = false;
+                this.error = ""; 
+                this.showsuccess = true;
+                this.success = res.message;
+                loadFirms().then(response => {
+                  this.company = response;
+                })
+              }
+
+          }).catch(error => {
+            console.log(error);
+          });
+        }
+    }
   }
 }
 </script>
--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 // ->middleware('auth:sanctum')
 Route::get('/company', [\App\Http\Controllers\API\CompanyController::class, 'company']);
 Route::post('/company/add', [\App\Http\Controllers\API\CompanyController::class, 'add']);
-Route::post('/company/delete/{id}', [\App\Http\Controllers\API\CompanyController::class, 'destroy']);
+Route::delete('/company/delete/{id}', [\App\Http\Controllers\API\CompanyController::class, 'destroy']);
 Route::post('/company/update/{id}', [\App\Http\Controllers\API\CompanyController::class, 'update']);
 
 Route::post('/worker/add', [\App\Http\Controllers\API\WorkerController::class, 'add']);
